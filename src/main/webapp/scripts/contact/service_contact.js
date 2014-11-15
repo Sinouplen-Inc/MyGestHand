1cc0ee45a6d6cc913aac5d78e87262ae806652b2
Sinouplen <sinouplen@gmail.com>
2014-11-15 23:22:06 +0100

    Upgrade version of jhipster

--- src/main/webapp/scripts/contact/service_contact.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-mygesthandJhipsterApp.factory('Contact', ['$resource',
-    function ($resource) {
-        return $resource('app/rest/contacts/:id', {}, {
-            'query': { method: 'GET', isArray: true},
-            'get': { method: 'GET'}
-        });
-    }]);
